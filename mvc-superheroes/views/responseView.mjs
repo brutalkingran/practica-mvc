165622024924d4e4d268c86e7bb366c312af5761
brutalkingran <swkvideogames@gmail.com>
2025-02-21 14:19:39 -0300

    correcciones ejercicio 2

--- mvc-superheroes/views/responseView.mjs
+++ mvc-superheroes/views/responseView.mjs
@@ -1,9 +1,7 @@
-import SuperheroesDataSource from "../repository/superheroesDataSource.mjs";
-
 export const renderizarSuperheroe = (superheroe) => {
     return JSON.stringify(superheroe, null, 2);
 }
 
-export const renderizarListaSuperheroes = (superheroe) => {
-    return JSON.stringify(SuperheroesDataSource, null, 2);
+export const renderizarListaSuperheroes = (superheroes) => {
+    return JSON.stringify(superheroes, null, 2);
 }
